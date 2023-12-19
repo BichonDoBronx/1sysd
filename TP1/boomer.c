fe1bf0fc4790509977c66b2efa7e841d669d495f
lucas <lucas.oliveira78190@gmail.com>
2023-12-19 14:05:39 +0100

    add boomer

---
+++ TP1/boomer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+ int main() {
+ 	int age; 
+ 
+ printf("votre age");
+ scanf("%d", &age);
+ 
+ if (age > 42 ) {
+ 	printf("Ok bommer\n");
+ } else {
+ 	printf("time to learn UNIX\n");
+ }
+ 	
+ 
+    exit(0);
+}
+
